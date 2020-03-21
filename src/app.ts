49c8361feed85d70db5ba89eacdb5b9f4b94a740
Alois Carrera <aloiscarrera@gmail.com>
2020-03-21 08:48:49 -0500

    Setting up basic server

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors'
 
+// Start
 const app = express();
+
+// Set-up
+
+app.set('port',process.env.port || 3000);
+
+// Middlewares
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// Routes
+
+app.get('/', (req, res) => {
+    res.send(`API is in http://localhost:${app.get('port')}`);
+})
+
+export default app;
